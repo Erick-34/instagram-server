36d8adad1a14c1116c5d226f32c683f43bd10e0b
Erick-34 <erickoliveira2060@gmail>
2020-08-27 11:02:37 -0300

    refactor: refactoning and add comment

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,17 +1,23 @@
-const { Comment } = require("../models/index");
+const { Comment, Post } = require("../models/index");
 const express = require("express");
 const router = express.Router();
 
-const idUser = req.session.currentUser._id;
-
 router.post("/comment", async (req, res) => {
   const { post, comment } = req.body;
+  const idUser = req.session.currentUser._id;
 
   const resposeCreatePost = await Comment.create({
-    comment: comment,
-    post: post,
+    comment,
+    post,
     author: idUser,
   });
 
-  res.status(200).json(resposeCreatePost);
+  await Post.updateOne(
+    { _id: post },
+    { $push: { comments: resposeCreatePost } }
+  ).populate("Post");
+
+  res.status(200).json({ success: true, data: resposeCreatePost });
 });
+
+module.exports = router;
