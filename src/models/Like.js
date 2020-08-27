2f80b16a9daf7d298bde749d30c46705d8b0a7f8
Erick-34 <erickoliveira2060@gmail>
2020-08-27 11:01:45 -0300

    feat: final LikeRouter

--- src/models/Like.js
+++ src/models/Like.js
@@ -3,14 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const likeSchema = new Schema({
-  post: {
-    type: Schema.Types.ObjectId,
-    ref: "Post",
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
+  post: String,
+  user: String,
 });
 
 module.exports = mongoose.model("Like", likeSchema);
